9393c6aa4180ade52bd8f240227e96c2bfcd0f79
Lazlo Mita Delgadillo <lazlo.mita@healthwise.org>
2016-02-14 12:30:06 -0400

    End of lesson 6 week 1, array of images, templates, helpers on Meteor.

--- image_share.js
+++ image_share.js
@@ -1,5 +1,15 @@
 if (Meteor.isClient) {
-	console.log("I am the client");
+	var img_data = [{
+		img_src:"laptops.jpg",
+		img_alt:"some laptops"
+	},{
+		img_src:"work.gif",
+		img_alt:"kermit working"
+	},{
+		img_src:"carnival-2016.jpg",
+		img_alt:"osos facld carnival oruro"
+	}];
+	Template.images.helpers({images:img_data});
 }
 
 if (Meteor.isServer) {
